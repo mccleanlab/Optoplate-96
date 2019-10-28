3b7bcdd90f7f83a15d431b59d773d131547eb54e
EdvardGrodem <edvard.grodem@gmail.com>
2019-10-28 10:01:50 -0500

    Fixed C syntex

--- Matlab/ExperimentGenerator.m
+++ Matlab/ExperimentGenerator.m
@@ -11,11 +11,12 @@ end
  save('phaseData.mat', 'phaseData');
  
  load('phaseData.mat')
- fileID = fopen('experiment_config.h','w');
+ fileID = fopen('../src/experiment_config.h','w');
  fprintf(fileID,'#ifndef _EXPERIMENT_CONFIG_H\n#define _EXPERIMENT_CONFIG_H\n\n');
+ fprintf(fileID, '#define PHASE_NUMB %i\n', size(phaseData(1).intens,1));
  fprintf(fileID,'#include "LED.h"\n\n');
  
- fprintf(fileID, 'uint8_t intensities[][%d] = {\n', size(phaseData(1).intens,1));
+ fprintf(fileID, 'uint8_t intensities[][PHASE_NUMB] = {\n');
  
  for i = [1:96]
     fprintf(fileID, '\t{');
@@ -24,7 +25,7 @@ end
  end
  fprintf(fileID, '};\n\n');
  
-  fprintf(fileID, 'uint16_t durations[][%d] = {\n', size(phaseData(1).duration, 1));
+  fprintf(fileID, 'uint16_t durations[][PHASE_NUMB] = {\n');
  
  for i = [1:96]
     fprintf(fileID, '\t{');
@@ -36,7 +37,7 @@ end
    fprintf(fileID, 'LED leds[] = {\n');
  
  for i = (1:96)
-    fprintf(fileID, '\tLED(& intensities[%i], & durations[%i]),\n', i, i);
+    fprintf(fileID, '\tLED(intensities[%i], durations[%i]),\n', i, i);
  end
  fprintf(fileID, '};\n\n');
  
