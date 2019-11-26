c9e089f600817cf651bc695c7f8fa4523a5c718a
EdvardGrodem <edvard.grodem@gmail.com>
2019-11-26 14:30:51 -0600

    Changed experiment

--- Matlab/CalibrationExperiment.m
+++ Matlab/CalibrationExperiment.m
@@ -4,12 +4,12 @@ phaseData = repmat(led,1,96);
 width = 1;
 exposureTime = 2;
 
-for i = (1:96)
-    phaseData(i).intensity = ones([width, 1])*255;
-    phaseData(i).periods = ones([width, 1])*1;
-    phaseData(i).offset = ones([width, 1])*(5 + i*(exposureTime+1)) ;
-    phaseData(i).tInterpulse = ones([width, 1])*1;
-    phaseData(i).tPulse = ones([width, 1])*exposureTime;
-end
 
+for i = (1:96)
+    phaseData(i).intensity = ones([width, 1])*2;
+    phaseData(i).periods = ones([width, 1])*1;
+    phaseData(i).offset = ones([width, 1]);
+    phaseData(i).tInterpulse = ones([width, 1])*1;
+    phaseData(i).tPulse = ones([width, 1])*60*30;
+end
  save('phaseData.mat', 'phaseData');
