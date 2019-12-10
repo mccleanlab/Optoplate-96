30dd7ee8932a5472537a67e182b1f75e4d343bc5
EdvardGrodem <edvard.grodem@gmail.com>
2019-12-10 10:40:33 -0600

    Update UniformExperiment.m

--- Matlab/UniformExperiment.m
+++ Matlab/UniformExperiment.m
@@ -1,15 +1,19 @@
 led.intensity = 0;
-led.duration = 0;
+led.periods = 0;
+led.offset = 0;
+led.tInterpulse = 0;
+led.tPulse = 0;
 phaseData = repmat(led,1,96);
 
+phases = 25;
 
 for i = (1:96)
-    phaseData(i).intensity = ones([10, 1])*1;
-    phaseData(i).periods = ones([10, 1])*100;
-    phaseData(i).offset = ones([10, 1])*2;
+    phaseData(i).intensity = ones([phases, 1])*1;
+    phaseData(i).periods = ones([phases, 1])*100;
+    phaseData(i).offset = ones([phases, 1])*2;
     
-    phaseData(i).tInterpulse = ones([10, 1])*3;
-    phaseData(i).tPulse = ones([10, 1])*1000;
+    phaseData(i).tInterpulse = ones([phases, 1])*3;
+    phaseData(i).tPulse = ones([phases, 1])*1000;
 end
 
  save('phaseData.mat', 'phaseData');
