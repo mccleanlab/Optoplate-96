bca35c6cb2c00a893cb053cfaa7cfc124bd51f65
kieransddd <ksweeney2@wisc.edu>
2020-03-04 22:27:24 -0600

    Measure without
    Calibration script will now just measure or plot input/output relationships

--- calibration/optoPlate_microscope_calibration.m
+++ calibration/optoPlate_microscope_calibration.m
@@ -1,8 +1,12 @@
 clearvars;clc; close all;
 %%
-amp_thresh = 0.25; % Fraction of max intensity threshold for segmenting wells
+amp_thresh = 0.05; % Fraction of max intensity threshold for segmenting wells
 min_peak_dist = 2;
 num_wells = 48;
+%% 
+input_values = [];
+input_values = repmat(20:20:240,4,1);
+input_values = reshape(input_values',48,1);
 
 %% Load
 [filenames, path] = uigetfile('.csv','Select files','Multiselect','On');
@@ -36,17 +40,28 @@ cmap = lines(num_wells);
 for f = 1:nFiles
     file = filenames{f};
     
-    LED = regexp(file,'LED01|LED02','match');
-    LED = regexp(LED,'\d*','match');
-    LED = str2double(LED{:});
+    if contains(file,'LED')
+        LED = regexp(file,'LED01|LED02|LED00','match');
+        LED = regexp(LED,'\d*','match');
+        LED = str2double(LED{:});
+    else
+        LED = 0;
+    end
+    
     
     well_set = regexp(file,'_WELLS\d*','match');
     well_set = string(regexp(well_set,'\d*','match'));
     well_set = str2double(well_set{:});
     
-    cal_round = regexp(file,'ROUND\d*_','match');
-    cal_round = string(regexp(cal_round,'\d*','match'));
-    cal_round = str2double(cal_round{:});
+    
+    
+    if contains(file,'ROUND')
+        cal_round = regexp(file,'ROUND\d*_','match');
+        cal_round = string(regexp(cal_round,'\d*','match'));
+        cal_round = str2double(cal_round{:});
+    else
+        cal_round = 0;
+    end
     
     opts = detectImportOptions([path file]);
     opts = setvartype(opts, 'char');
@@ -65,12 +80,10 @@ for f = 1:nFiles
     % Preprocess measurement data by thresholding and background subtraction
     time = measurements_temp.Sample';
     irradiance = measurements_temp.Irradiance_raw;
-%     hold on; plot(time,irradiance)
     
     irradiance_BG = median(irradiance(irradiance<amp_thresh*max(irradiance))); % Calculate background intensity
     irradiance = irradiance - irradiance_BG; % Subtract out background intensity
     irradiance(irradiance<amp_thresh*max(irradiance)) = nan; % Threshold to exclude intensity data from outside wells
-%     plot(time,irradiance)
     
     % Create binary irradiance signal for peak finding
     npad = 5;
@@ -88,7 +101,6 @@ for f = 1:nFiles
     end
     
     irradiance_binary = irradiance_binary(1:end-npad);
-%     plot(time,5*irradiance_binary/1E6)
     
     % Calculate intensities for each identified well
     for j = 1:num_wells
@@ -97,8 +109,6 @@ for f = 1:nFiles
         outliers = irradiance_temp(isoutlier(irradiance_temp));
         irradiance(ismember(irradiance,outliers) & well_idx==j) = nan;
     end
-%     plot(time,irradiance,'*')
-%     gscatter(time,irradiance,well_idx,cmap)
     
     measurements_temp.Irradiance = irradiance;
     measurements_temp.Well_idx = well_idx;
@@ -115,7 +125,6 @@ for f = 1:nFiles
     
     measurements_temp = join(measurements_temp,idx2well);
     measurements{f,1} = measurements_temp;
-    
 end
 
 measurements = vertcat(measurements{:});
@@ -127,106 +136,133 @@ LED.Properties.RowNames={}; LED.GroupCount = [];
 idx = contains(LED.Properties.VariableNames,'Irradiance');
 LED.Properties.VariableNames(idx) = {'Irradiance'};
 
-% Calculate calibration values
-% Interleave LED intensities following 96 well layout
-maxCal = 255;
-
-% Format intensities for optoPlate
-intensities(:,1) = LED.Irradiance(LED.LED==1);
-intensities(:,2) = LED.Irradiance(LED.LED==2);
-
-% Format intensities for display
-intensities_display(1:2:191) = intensities(:,1);
-intensities_display(2:2:192) = intensities(:,2);
-intensities_display = reshape(intensities_display,24,8)';
-intensities_display = intensities_display;
-
-% Scale intensities by calibration values from previous round (if applicable)
-if cal_round == 1
-    intensities_round_1 = intensities;
-    save('intensities_round_1','intensities_round_1');
-    cal_previous = ones([96, 2])*255;
-    minIntensity = min(intensities(:));
-    a = 1;
-else
-    % Load previous values
-    cal_previous = load(['cal_round_' num2str(cal_round - 1)]);
-    cal_previous = cal_previous.cal;
+if cal_round==0 && ~isempty(input_values)
     
-    intensities_round_1 = load('intensities_round_1');
-    intensities_round_1 = intensities_round_1.intensities_round_1;
-    minIntensity = min(intensities_round_1(:));
-    a = 0.6;
+    LED.input_values = input_values;
+    
+elseif cal_round~=0 % Calculate calibration values
+    
+    % Interleave LED intensities following 96 well layout
+    maxCal = 255;
+    
+    % Format intensities for optoPlate
+    intensities(:,1) = LED.Irradiance(LED.LED==1);
+    intensities(:,2) = LED.Irradiance(LED.LED==2);
+    
+    % Format intensities for display
+    intensities_display(1:2:191) = intensities(:,1);
+    intensities_display(2:2:192) = intensities(:,2);
+    intensities_display = reshape(intensities_display,24,8)';
+    intensities_display = intensities_display;
+    
+    % Scale intensities by calibration values from previous round (if applicable)
+    if cal_round == 1
+        intensities_round_1 = intensities;
+        save([path 'intensities_round_1'],'intensities_round_1');
+        cal_previous = ones([96, 2])*255;
+        minIntensity = min(intensities(:));
+        a = 1;
+    else
+        % Load previous values
+        cal_previous = load([path 'cal_round_' num2str(cal_round - 1)]);
+        cal_previous = cal_previous.cal;
+        
+        intensities_round_1 = load([path 'intensities_round_1']);
+        intensities_round_1 = intensities_round_1.intensities_round_1;
+        minIntensity = min(intensities_round_1(:));
+        a = 0.6;
+    end
+    
+    % Calculate calibration values
+    cal = cal_previous/maxCal - a*(intensities - minIntensity)./intensities_round_1;
+    cal = round(cal*maxCal);
+    
+    cal(cal>255) = 255;
+    cal(cal<0) = 0;
+    
+    % Reformat calibration values for display
+    cal_display(1:2:191) = cal(:,1);
+    cal_display(2:2:192) = cal(:,2);
+    cal_display = reshape(cal_display,24,8)';
+    
+    % Save calibration values
+    save([path 'cal_round_' num2str(cal_round)],'cal');
 end
 
-% Calculate calibration values
-cal = cal_previous/maxCal - a*(intensities - minIntensity)./intensities_round_1;
-cal = round(cal*maxCal);
-
-cal(cal>255) = 255;
-cal(cal<0) = 0;
-
-% Reformat calibration values for display
-cal_display(1:2:191) = cal(:,1);
-cal_display(2:2:192) = cal(:,2);
-cal_display = reshape(cal_display,24,8)';
-
-% Save calibration values
-save(['cal_round_' num2str(cal_round)],'cal');
 
 %% Plot
 close all
 figure('Position',[100 100 1200 800])
 
-ymax = 1.25*max(LED.Irradiance);
-rowlist = 'A':'H';
-column_list = string(cellfun(@(x) sprintf('%02d',x),num2cell(1:12),'UniformOutput',false));
-xlabeldisp(1:2:23) = string(1:12);
-xlabeldisp(2:2:24) = repmat("",1,12);
-
-% Heatmap of intensities following 96-well layout
-subplot(2,1,1);
-h = heatmap(intensities_display);
-h.YDisplayLabels = cellstr(rowlist(:));
-h.XDisplayLabels = xlabeldisp;
-h.Title = 'Mean LED intensity';
-
-% Heatmap of calibration values following 96-well layout
-subplot(2,1,2);
-h = heatmap(cal_display);
-h.YDisplayLabels = cellstr(rowlist(:));
-h.XDisplayLabels = xlabeldisp;
-h.Title = ['Calibration values: round ' num2str(cal_round)];
-savefig(gcf,['heatmap_round_' num2str(cal_round)]);
-
-% Plot irradiance
-clear g; figure
-g = gramm('x',cellstr(LED.Well),'y',LED.Irradiance,...
-    'color',cellstr(regexp(LED.Well,'[a-zA-Z]*','match')),'subset',~isnan(LED.Irradiance));
-g.facet_grid(LED.LED,[]);
-g.geom_point();
-g.set_title(['Irradiance per well: round ' num2str(cal_round)]);
-g.axe_property('XTickLabelRotation',60,'YLim',[0 ymax],'Xlim',[0 97]);
-g.set_names('x','Well','y', ['Intensity' newline '(mean ± std)'],'Row','LED','Color','Row','Marker','Orientation');
-g.draw();
-savefig(gcf,['intensities_round_' num2str(cal_round)]);
+if cal_round==0 && ~isempty(input_values)
+    clear g; close all; figure('Position',[100 100 1200 800])
+    g = gramm('x',LED.input_values,'y',LED.Irradiance,'subset',~isnan(LED.Irradiance));
+    g.stat_summary('type','std','geom',{'point','black_errorbar'});
+    g.set_title('Irradiance vs input value');
+    g.set_names('x','Input value','y', ['Intensity' newline '(mean ± std)']);
+    g.stat_glm('disp_fit',true)
+    g.draw();
+    
+    glm = fitglm(LED.input_values,LED.Irradiance); disp(glm)
+    rsquared = glm.Rsquared.Ordinary; disp(rsquared)
+    
+elseif cal_round~=0
+    clear g; close all; figure('Position',[100 100 1200 800])
+    
+    ymax = 1.25*max(LED.Irradiance);
+    rowlist = 'A':'H';
+    column_list = string(cellfun(@(x) sprintf('%02d',x),num2cell(1:12),'UniformOutput',false));
+    xlabeldisp(1:2:23) = string(1:12);
+    xlabeldisp(2:2:24) = repmat("",1,12);
+    
+    % Heatmap of intensities following 96-well layout
+    subplot(2,1,1);
+    h = heatmap(intensities_display);
+    h.YDisplayLabels = cellstr(rowlist(:));
+    h.XDisplayLabels = xlabeldisp;
+    h.Title = 'Mean LED intensity';
+    
+    % Heatmap of calibration values following 96-well layout
+    subplot(2,1,2);
+    h = heatmap(cal_display);
+    h.YDisplayLabels = cellstr(rowlist(:));
+    h.XDisplayLabels = xlabeldisp;
+    h.Title = ['Calibration values: round ' num2str(cal_round)];
+    savefig(gcf,[path 'heatmap_round_' num2str(cal_round)]);
+    
+    % Plot irradiance
+    clear g; figure
+    g = gramm('x',cellstr(LED.Well),'y',LED.Irradiance,...
+        'color',cellstr(regexp(LED.Well,'[a-zA-Z]*','match')),'subset',~isnan(LED.Irradiance));
+    g.facet_grid(LED.LED,[]);
+    g.geom_point();
+    g.set_title(['Irradiance per well: round ' num2str(cal_round)]);
+    g.axe_property('XTickLabelRotation',60,'YLim',[0 ymax],'Xlim',[0 97]);
+    g.set_names('x','Well','y', ['Intensity' newline '(mean ± std)'],'Row','LED','Color','Row','Marker','Orientation');
+    g.draw();
+    savefig(gcf,[path 'intensities_round_' num2str(cal_round)]);
+end
 
 %% Plate stats
-optoPlate_stats = table();
-optoPlate_stats.cal_round = num2str(cal_round);
-optoPlate_stats.mean = mean(LED.Irradiance);
-optoPlate_stats.std = std(LED.Irradiance);
-optoPlate_stats.CV = 100*optoPlate_stats.std/optoPlate_stats.mean;
-optoPlate_stats.max = max(LED.Irradiance);
-optoPlate_stats.min = min(LED.Irradiance);
+if cal_round~=0
+    optoPlate_stats = table();
+    optoPlate_stats.cal_round = num2str(cal_round);
+    optoPlate_stats.mean = mean(LED.Irradiance);
+    optoPlate_stats.std = std(LED.Irradiance);
+    optoPlate_stats.CV = 100*optoPlate_stats.std/optoPlate_stats.mean;
+    optoPlate_stats.max = max(LED.Irradiance);
+    optoPlate_stats.min = min(LED.Irradiance);
+    
+    optoPlate_stats
+end
 
-optoPlate_stats
 %%
-measurements_out.round = cal_round;
-measurements_out.measurements = measurements;
-measurements_out.LED = LED;
-measurements_out.cal = cal;
-measurements_out.optoPlate = optoPlate_stats;
-
-save(['measurements_round_' num2str(cal_round)],'measurements_out');
-
+if cal_round~=0
+    measurements_out.round = cal_round;
+    measurements_out.measurements = measurements;
+    measurements_out.LED = LED;
+    measurements_out.cal = cal;
+    measurements_out.optoPlate = optoPlate_stats;
+    
+    save([path 'measurements_round_' num2str(cal_round)],'measurements_out');
+end
