0f82c8f68ceb14864f70dd192138204343733819
EdvardGrodem <edvard.grodem@gmail.com>
2020-04-20 12:29:30 +0200

    Tested new PWM

---
+++ experiment/test.m
@@ -0,0 +1,4 @@
+%createExperiment(randi([0, 255], 8, 12), randi([1, 10], 8, 12),randi([0, 255], 8, 12),randi([20, 1000], 8, 12),randi([20, 1000], 8, 12),randi([1, 40], 8, 12),randi([1, 50], 8, 12))
+figure();
+plotLedPattern(20, 2, 10, 200, 300, 10, 20)
+plotLedGui(experiment)
