ad1c10313847d763a46a0ef9dce66d39181a16db
EdvardGrodem <edvard.grodem@gmail.com>
2020-06-23 12:55:41 +0200

    Added example for multicolor

--- experiment/Example.m
+++ experiment/Example.m
@@ -1,34 +1,32 @@
-% This file creates a checker pattern that stays on for 10 seconds, off for
-% 1 second, and repeats this 5 times.
+%% Example - OptoPlate
+% This code provide an example on how to create a experiment file for the
+% OptoPlate configured with LEDs of one color.
+
 addpath('visualization');
 
-subPulses = true; % Enable/disable subpulses
+subPulses = false; % Enable/disable subpulses
 
-amplitudes= zeros(2, 8, 12); % Turn every other LED on
-for k = 1:2
-    for i = 1:8
-       for j = 1:12
-           amplitudes(k, i, j) = mod(i+j, 2)*20;
-       end
-    end
+% Set a nonzero amplitude on every other LED
+amplitudes= zeros(8, 12);
+for i = 1:8
+   for j = 1:12
+       amplitudes(i, j) = mod(i+j, 2)*20;
+   end
 end
 
-amplitudes= ones(2, 8, 12)*10; % Turn every other LED on
-
-
 % Loop through the pulses 5 times
-pulse_numbs = ones(2, 8, 12)* 5; 
+pulse_numbs = ones(8, 12)* 5; 
 % Wait 2 sec before starting pulse sequence
-pusle_start_times = [ones(1,8, 12)*1; ones(1, 8, 12)*2];
+pusle_start_times = ones(8, 12)*2;
 % Let LED be high for 10 seconds
-pulse_high_times = ones(2, 8, 12)*10;
-% Pause for 1 second between each pulse
-pulse_low_times = ones(2, 8, 12)*4;
+pulse_high_times = ones(8, 12)*10;
+% Pause for 4 second between each pulse
+pulse_low_times = ones(8, 12)*4;
 
 if subPulses
     % During LED high time, let the LED be on for 1 sec and off for 2 sec
-    subpulse_high_times = ones(2, 8, 12)*1;
-    subpulse_low_times = ones(2, 8, 12)*1;
+    subpulse_high_times = ones(8, 12)*1;
+    subpulse_low_times = ones(8, 12)*2;
     
     % Create and save experiment
     ex = createExperiment(amplitudes, pulse_numbs, pusle_start_times, pulse_high_times, pulse_low_times,subpulse_high_times, subpulse_low_times);
